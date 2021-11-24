18e60032d9d8f1d7e561594eb746d0dcb93e929f
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-24 18:52:53 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    throw new HttpException(
+      { success: false, message: 'api is broken' },
+      HttpStatus.FORBIDDEN,
+    );
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'partialCat';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
