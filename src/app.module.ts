be6bf41ad2ef3342c3a52ed3be02f3a0a1f7135b
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-24 19:21:52 +0900

    mongoos 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://yeum_sang_kwon:0523a0523a@nestcluster.8mtqp.mongodb.net/test',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
