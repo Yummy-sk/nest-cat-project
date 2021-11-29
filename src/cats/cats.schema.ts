65f10628ffb9a210b347c5a43cd62ead42906e9a
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-11-30 00:41:01 +0900

    스키마에 prop 추가

--- src/cats/cats.schema.ts
+++ src/cats/cats.schema.ts
@@ -1,16 +1,27 @@
-import { SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Prop, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
 
 const options: SchemaOptions = {
   timestamps: true,
 };
 
 export class Cat extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+  })
   email: string;
 
+  @Prop({
+    required: true,
+  })
   name: string;
 
+  @Prop({
+    required: true,
+  })
   password: string;
 
+  @Prop()
   imgUrl: string;
 }
 
