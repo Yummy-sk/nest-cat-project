4e867d988e50c63e36d814686e8fe32fdd37e77f
Yeum Sang Kwon <kunshup2000@gmail.com>
2021-12-02 15:59:29 +0900

    virtual fild를 통해 민감한 정보는 숨기기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CatsService } from './cats/cats.service';
 
 @Controller()
 export class AppController {
